025544d81023d0994fcc08dc1810890922f76fa9
tachibis <chibisova12@gmail.com>
2026-09-18 13:00:34 +0900

    Initial Commit

---
+++ gptWidget/gptWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  gptWidgetApp.swift
+//  gptWidget
+//
+//  Created by Tanya Chibis on 18.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct gptWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
